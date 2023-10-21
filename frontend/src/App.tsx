02f4832189951ce2ce7b915bbe9984573ec239c2
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-10-21 20:47:47 -0300

    Create teste typescript react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { IProduct } from './App.models';
+import { Product } from './Product';
+import { PurchaseDisplay } from './PurchaseDisplay';
+
+
+
+const initialProducts: IProduct[] = 
+  [
+    {name: 'Keyboard', image: 'https://picsum.photos/200/300', price: 200},
+    {name: 'Headphones', image: 'https://picsum.photos/200/300', price: 300},
+    {name: 'mouse', image: 'https://picsum.photos/200/300', price: 100}
+  ]
+
+  
 
 function App() {
+
+  const [products, setProducts] = useState(initialProducts)
+  const [isProductBought, setIsProductBought] = useState(false)
+  const [productBought, setProductBought] = useState({name:'', image: '', price: 0})
+
+  const handlePurchase = (product:IProduct) => {
+    setProductBought(product)
+    setIsProductBought(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='Tiny-store'>Tiny Store</h1>
+      {
+        (isProductBought) ? 
+          (<PurchaseDisplay productPurchased={productBought} />) 
+          : 
+          ( <div className={'App-container'}>
+            {
+              products.map(product => {
+                return (
+                  <Product product={product} onBuy={handlePurchase} key={product.name}/>   
+                )
+              })
+            }
+          </div>
+          )}
+    </>
   );
 }
 
